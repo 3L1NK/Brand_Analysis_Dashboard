4467bc0c1c9301857118729cd5295e8e2ce59a52
Syahreza <syahrezaraditya@gmail.com>
2024-06-17 19:17:21 +0200

    update dashboard and donut chart

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ from datetime import datetime
 from assets.styles import NAVBAR_STYLE, LAYOUT_STYLE, HEADER_TITLE_STYLE
 
 # inital variable for neutrogena
-df_neutrogena = pd.read_csv('./reddit_sentiment/neutrogena.csv')
+df_neutrogena = pd.read_csv('./youtube_sentiment/youtube_comments_with_sentiment.csv')
+
+print(df_neutrogena)
 
 # components
 navbar = dbc.Navbar(
@@ -28,9 +30,17 @@ navbar = dbc.Navbar(
     style=NAVBAR_STYLE
 )
 
+pie_chart_fig = px.pie(
+    df_neutrogena['Sentiment'].value_counts().reset_index(), 
+    values='count', 
+    names='Sentiment', 
+    title='Sentiment Distribution',
+    color_discrete_sequence=px.colors.qualitative.Set3
+)
+
 pie_chart_container = dbc.Container(
   children=[
-    "pie charts"
+    pie_chart_fig
   ]
 )
 
@@ -39,6 +49,43 @@ sentiment_plot_container = dbc.Container(
     "sentiment line plot"
   ]
 )
+
+
+comments_table = dash_table.DataTable(
+    id='comments-table',
+    columns=[
+        {'name': 'Comments', 'id': 'Comments'},
+        {'name': 'Sentiment', 'id': 'Sentiment'},
+        {'name': 'Sentiment Score', 'id': 'Sentiment Score'}
+    ],
+    page_current=0,
+    page_size=10,
+    page_action='custom',
+    style_table={'overflowX': 'auto'},
+    style_cell={
+        'whiteSpace': 'normal',
+        'height': 'auto',
+        'textAlign': 'left',
+        'paddingLeft': '10px',
+        'paddingRight': '10px',
+        'fontFamily': 'Arial, sans-serif',
+    },
+    style_data_conditional=[
+        {
+            'if': {'row_index': 'odd'},
+            'backgroundColor': 'rgb(248, 248, 248)'  # Light grey background for odd rows (striped effect)
+        }
+    ],
+)
+
+news_sentiment_plot = dbc.Container(
+    id='plot-container-news',
+    style={'padding': '0px'},
+    children=[
+        html.H4(children='Sentiment Analysis Visualization'),
+        dcc.Store(id='data-store-news'),
+        dcc.Graph(id='pred-graph-news'),
+    ])
 
 # initialise the dash application
 app = dash.Dash(
